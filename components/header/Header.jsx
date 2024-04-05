fbcc8e30bb80969808aa717501bbdf85836f7035
Scarlet Rosa <likedyourbody@gmail.com>
2024-04-05 19:43:55 +0200

    first commit

---
+++ components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./App.css";
+import Header from './components/header/Header';
+
+const Header = () => {
+    return (
+        <>
+        <Header />
+        
+        </>
+    )
+}
+
+export default Header
